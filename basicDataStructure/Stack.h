df2cab85e4574f46b7e588751bff36f5974da733
synnnnu <suyeonnohh@gmail.com>
2026-07-19 15:41:04 +0900

    refactor: 스택 int형 뿐만 아니라 모든 자료형 받을 수 있게 리팩토링

--- basicDataStructure/Stack.h
+++ basicDataStructure/Stack.h
@@ -1,19 +1,50 @@
 #ifndef STACK_H
 #define STACK_H
 
+template <typename T, int N = 100>
 class Stack {
 private:
-    int arr[100];
+    T arr[N];
     int count;
-    int capacity;
 public:
-    Stack(int cap = 100); // 생성자
-    bool push(int value);        // 성공 true / 가득 참 false
-    bool pop();                  // 제거만 함, 값은 리턴 안 함
-    int peek();
-    bool isEmpty();
-    bool isFull();
-    int size();
+    Stack() : count(0) {} // 생성자
+
+    bool push(const T& value) {
+        if (count == N) return false;
+
+        arr[count++] = value;
+        return true;
+    }
+
+    bool pop() {
+        if (count == 0) return false;
+        
+        count--;
+        return true;
+    }
+
+    bool peek(T& outValue) {
+        if (count == 0) return false;
+
+        outValue = arr[count-1];
+        return true;
+    }
+
+    bool isEmpty() {
+        if (count == 0) return true;
+    
+        return false;
+    }
+
+    bool isFull() {
+        if (count == N) return true;
+    
+        return false;
+    }
+
+    int size() {
+        return count;
+    }
 };
 
 #endif
